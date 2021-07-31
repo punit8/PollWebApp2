891b61d5ca9b1d188e95b073c97d4f6e5a6d7f36
Punit <punitg213@gmail.com>
2021-07-31 21:46:31 +0530

    registed db to admin

--- poll/poll/settings.py
+++ poll/poll/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls'
+    # 'polls'
 ]
 
 MIDDLEWARE = [
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC + 05:30'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
