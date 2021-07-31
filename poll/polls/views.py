f1df925a6d5dcc13b69dca908e1f39e53cdcc5b0
Punit <punitg213@gmail.com>
2021-07-31 22:18:08 +0530

    update view

--- poll/polls/views.py
+++ poll/polls/views.py
@@ -1,7 +1,20 @@
+from polls.models import Question
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("hello")
+    question_list = Question.objects.all()
+    output = ', '.join(q.question_text for q in question_list)
+
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s" %question_id)
+
+def results(request, question_id):
+    return HttpResponse("You are looking at result of question %s" %question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
 
